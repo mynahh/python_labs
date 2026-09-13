0334fbe2141957f1e5d012a95f9abd2aa5785ef4
mynahh <mikepak985@gmail.com>
2026-09-13 22:08:24 +0300

    modified:   src/lab01/03.py

---
+++ src/lab01/03.py
@@ -0,0 +1,7 @@
+price,discount,vat=map(float, input().split())
+base = price * (1 - discount/100)
+vat_amount = base * (vat/100)
+total = base + vat_amount
+print('База после скидки:', f'{base:.2f}', '₽')
+print('НДС:', f'{vat_amount:.2f}', '₽')
+print('Итого к оплате:', f'{total:.2f}', '₽')
