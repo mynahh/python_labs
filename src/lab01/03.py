6cfdaa16ead8a43e5c2afb2ae19f81e1b9611663
mynahh <mikepak985@gmail.com>
2026-09-15 11:40:52 +0300

    modified:   src/lab01/03.py

--- src/lab01/03.py
+++ src/lab01/03.py
@@ -3,5 +3,5 @@ base = price * (1 - discount/100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
 print('База после скидки:', f'{base:.2f}', '₽')
-print('НДС:', f'{vat_amount:.2f}', '₽')
-print('Итого к оплате:', f'{total:.2f}', '₽')
+print('НДС:','             ', f'{vat_amount:.2f}', '₽')
+print('Итого к оплате:','  ',f'{total:.2f}', '₽')
---
+++ src/lab01/05.py
@@ -0,0 +1,5 @@
+a,b,c=input().split()
+print('ФИО:',a, b, c,)
+
+print('Инициалы:'f'{a[0].upper()}{b[0].upper()}{c[0].upper()}')
+print('Длина (символов):', len(a)+len(b)+len(c)+2)#добавляю два пробела между фио
